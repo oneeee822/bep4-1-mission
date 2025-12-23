821c8207c739ae89a4a05b2a27053b2f2216c91d
OneE <bestoneeee@naver.com>
2025-12-23 14:17:44 +0900

    0005

--- src/main/java/com/back/boundedContext/member/service/MemberService.java
+++ src/main/java/com/back/boundedContext/member/service/MemberService.java
@@ -1,19 +1,17 @@
-package com.back.service;
+package com.back.boundedContext.member.service;
 
-import com.back.entity.Member;
-import com.back.exception.DomainException;
-import com.back.repository.MemberRepository;
+import com.back.boundedContext.member.entity.Member;
+import com.back.boundedContext.member.repository.MemberRepository;
+import com.back.global.exception.DomainException;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
 
 @Service
+@RequiredArgsConstructor
 public class MemberService {
     private final MemberRepository memberRepository;
-
-    public MemberService(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
 
     public long count() {
         return memberRepository.count();
