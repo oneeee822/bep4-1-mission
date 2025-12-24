334e504c2990a85e9e702632133daba2708bbce1
OneE <bestoneeee@naver.com>
2025-12-24 11:19:47 +0900

    0017

--- src/main/java/com/back/boundedContext/member/domain/Member.java
+++ src/main/java/com/back/boundedContext/member/domain/Member.java
@@ -1,6 +1,7 @@
 package com.back.boundedContext.member.domain;
 
 import com.back.global.jpa.entity.BaseIdAndTime;
+import com.back.shared.member.domain.SourceMember;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -11,20 +12,14 @@ import lombok.NoArgsConstructor;
 @Table(name = "MEMBER_MEMBER")
 @NoArgsConstructor
 @Getter
-public class Member extends BaseIdAndTime {
-    @Column(unique = true)
-    private String username;
-    private String password;
-    private String nickname;
-    private int activityScore;
-
+public class Member extends SourceMember {
     public Member(String username, String password, String nickname) {
-        this.username = username;
-        this.password = password;
-        this.nickname = nickname;
+        super(username, password, nickname);
     }
 
     public int increaseActivityScore(int amount){
-        return this.activityScore += amount;
+        setActivityScore(getActivityScore() + amount);
+
+        return getActivityScore();
     }
 }
