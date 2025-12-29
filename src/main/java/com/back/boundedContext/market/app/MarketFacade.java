50672db9415554a5d1b4ef74db590a8ed6c17fd6
OneE <bestoneeee@naver.com>
2025-12-29 09:42:03 +0900

    0032
    - Product 엔티티 및 JPA 리포지토리 추가
    - 마켓 상품 생성을 위한 MarketCreateProductUseCase 구현
    - 상품 개수 조회 및 회원 조회를 담당하는 MarketSupport 추가
    - 마켓 기능 진입점 역할의 MarketFacade 도입
      - 회원 동기화, 상품 생성, 상품 수 조회 기능 제공
    - 게시글 API 데이터를 활용한 초기 상품 데이터 생성 로직 추가

--- src/main/java/com/back/boundedContext/market/app/MarketFacade.java
+++ src/main/java/com/back/boundedContext/market/app/MarketFacade.java
@@ -1,18 +1,51 @@
 package com.back.boundedContext.market.app;
 
 import com.back.boundedContext.market.domain.MarketMember;
+import com.back.boundedContext.market.domain.Product;
 import com.back.shared.post.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
 public class MarketFacade {
+    private final MarketSupport marketSupport;
     private final MarketSyncMemberUseCase marketSyncMemberUsecase;
+    private final MarketCreateProductUseCase marketCreateProductUseCase;
 
     public MarketMember syncMember(MemberDto member){
         return marketSyncMemberUsecase.syncMember(member);
+    }
+
+    @Transactional
+    public Product createProduct(MarketMember seller,
+                                 String sourceTypeCode,
+                                 int sourceId,
+                                 String name,
+                                 String description,
+                                 int price,
+                                 int salePrice
+    ) {
+        return marketCreateProductUseCase.createProduct(
+                seller,
+                sourceTypeCode,
+                sourceId,
+                name,
+                description,
+                price,
+                salePrice
+        );
+    }
+
+    public long productsCount() {
+        return marketSupport.countProducts();
+    }
+
+    public Optional<MarketMember> findMemberByUsername(String username) {
+        return marketSupport.findMemberByUsername(username);
     }
 }
